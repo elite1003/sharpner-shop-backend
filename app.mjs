d7cf0b392506abc8dbeb42cf3e954feafa82ae5a
elite1003 <sudhirkumar10397@gmail.com>
2024-05-11 00:51:04 +0530

    add body-parse to parse incomming json/text/urlencoded/raw data.

--- app.mjs
+++ app.mjs
@@ -1,12 +1,23 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("inside first middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/add-product", (req, res, next) => {
+  res.send(
+    `<form action = "/product" method = "POST"><label>name :</label><input type = "text" name= "name"/> 
+    <label>size :</label><input type = "text" name = "size"/><button type = "submit">Add Product</button></form>`
+  );
 });
-app.use("/", (req, res, next) => {
-  res.send({ key: "value" });
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello from Home express</h1>");
 });
 app.listen(4000, () => {
   console.log("Server running on port 4000");
